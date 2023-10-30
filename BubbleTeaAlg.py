3028152cb5bee30596f1ba60a6d7a72925f84c13
Jonathan Feng <Jonathan1964622@outlook.com>
2023-10-30 12:30:00 -0700

    BubbleTea

--- BubbleTeaAlg.py
+++ BubbleTeaAlg.py
@@ -9,18 +9,47 @@
 
 # Options: CoCo, Chatime, Suntea, Xing fu Tang
 
+NUM_RESPONDENTS = 5
+
 coco_likes = 0
+chatime_likes = 0
 suntea_likes = 0
-chattime_likes = 0
-xing_likes = 0
+other = 0
+xingfutang_likes = 0
+bbqueen_likes = 0
 
-print("What's your favourite bubble tea place?")
+for _ in range(NUM_RESPONDENTS):
+    print("What's your favourite bubble tea place?")
 
-fave_bbt = input().strip(",.?! ").lower()
+    fave_bbt = input().strip(",.?! ").lower()
 
-if fave_bbt == "coco":
-    coco_likes += 1
-elif fave_bbt == "chatime":
-    chattime_likes += 1
-elif fave_bbt == "suntea":
-    suntea_likes += 1
+    if fave_bbt == "coco":
+        coco_likes += 1
+    elif fave_bbt == "chatime":
+        chatime_likes += 1
+    elif fave_bbt == "suntea":
+        suntea_likes += 1
+    elif fave_bbt == "xingfutang":
+        xingfutang_likes += 1
+    elif fave_bbt == "bbqueen":
+        bbqueen_likes += 1
+    else:
+        other += 1
+
+# Print a summary of the results
+coco_percentage = coco_likes / NUM_RESPONDENTS * 100
+chatime_percent = chatime_likes / NUM_RESPONDENTS * 100
+suntime_percent = suntea_likes / NUM_RESPONDENTS * 100
+xing_percent = xingfutang_likes / NUM_RESPONDENTS * 100
+bbqueen_percent = bbqueen_likes / NUM_RESPONDENTS * 100
+other_percent = other / NUM_RESPONDENTS * 100
+
+print(f"CoCo Likes: {coco_likes}   Percentage: {round(coco_percentage, 2)}%")
+print(f"chatime Likes: {chatime_likes}   Percentage: {round(chatime_percent, 2)}%")
+print(f"Suntea Likes: {suntea_likes}   Percentage: {round(suntime_percent, 2)}%")
+print(f"xingfutang Likes: {xingfutang_likes}   Percentage: {round(xing_percent, 2)}%")
+print(f"bbqueen Likes: {bbqueen_likes}   Percentage: {round(bbqueen_percent, 2)}%")
+print(f"Other Likes: {other}   Percentage: {round(other_percent, 2)}%")
+
+
+
