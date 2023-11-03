9c657d78876d455a8a4fde9ffa5a092b7aa92fb9
Jonathan Feng <Jonathan1964622@outlook.com>
2023-11-03 11:43:36 -0700

    Added work

---
+++ Age_in_2049.py
@@ -0,0 +1,4 @@
+age = input("How old are you? ").strip(",.?! ")
+age = int(age)
+future_age = age + 26
+print(f"You will be {future_age} years old in 2049") 
---
+++ Olympic_judging.py
@@ -0,0 +1,10 @@
+judge1 = int(input("Judge 1: "))
+judge2 = int(input("Judge 2: "))
+judge3 = int(input("Judge 3: "))
+judge4 = int(input("Judge 4: "))
+judge5 = int(input("Judge 5: "))
+
+average = judge1 + judge2 + judge3 + judge4 + judge5
+average = average / 5
+print(f"Your olympic score is {average}")
+
