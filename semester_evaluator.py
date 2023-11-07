d59476f5b90a42267ef5ebe4dcb907bbb15f2494
Jonathan Feng <Jonathan1964622@outlook.com>
2023-11-07 10:01:49 -0800

    added spaces

--- semester_evaluator.py
+++ semester_evaluator.py
@@ -1,14 +1,18 @@
-#credits to jonathan
+#jonathan
+
 sem_courses = input("how many courses are you taking this semester")
 sem_courses = int(sem_courses)
 scores = []
+
 for i in range(sem_courses):
     value = input(f"how do you rate your course {i + 1} out of 5?")
     scores.append(value)
 total = 0
+
 for i in range(sem_courses):
     total += int(scores[i])
 final_score = total/sem_courses
+
 if final_score < 1:
     print(f"{final_score}? Ouch")
 elif final_score < 3:
