11dfcd6d477b3e068aab7f92efca19fb9a34efd8
Jonathan Feng <Jonathan1964622@outlook.com>
2024-01-22 10:44:53 -0800

    added

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+#dictionary
+
+# this is how you make a dictionary
+This_is_dictionary = {
+    "name": "bob",
+    "sport": "soccer",
+    "Fav_colours": ["blue", "red"],
+    "Bool_value": False
+}
+
+# this is how you access a dictionary
+print(This_is_dictionary["name"])
+
+x = This_is_dictionary["Fav_colours"]
+print(x)
+
+value = This_is_dictionary["Bool_value"]
+print(value)
+
